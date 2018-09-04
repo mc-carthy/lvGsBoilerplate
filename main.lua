dc2f1f68381325575b79ab91f616efc7857b159d
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-09-04 20:23:41 +0100

    Add scale factor for non-GS development

--- main.lua
+++ main.lua
@@ -1,20 +1,23 @@
+SCALE_FACTOR = 1
+
 function love.load()
-	key = ""
-end		
+    key = ""
+    love.window.setMode(love.graphics.getWidth() * SCALE_FACTOR, love.graphics.getHeight() * SCALE_FACTOR)
+end
 
 function love.update()
-
 end
 
 function love.keypressed(k)
-	if k == "escape" then
-		os.exit()
-	else
-		key = tostring(k)
-	end
+    if k == "escape" then
+        os.exit()
+    else
+        key = tostring(k)
+    end
 end
 
 function love.draw()
+    love.graphics.scale(SCALE_FACTOR)
     local major, minor, revision, codename = love.getVersion()
     local str = string.format("Version %d.%d.%d - %s", major, minor, revision, codename)
     love.graphics.print(str, 20, 20)
